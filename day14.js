f0ccd3af7a12b16a4056ecc88195179f6b6d9496
divine016 <koutidivine@gmail.com>
2023-04-30 01:57:07 +0100

    started learning about intereaction using html and js

---
+++ day14.js
@@ -0,0 +1,5 @@
+//
+document.getElementById('post').textContent = 'My first project on grasshopper'
+document.getElementById('description').textContent = 'i am building my first webpage using grasshopper tutorial.'
+let details = document.getElementById('details')
+details.textContent = " this is so cool"
