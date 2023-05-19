36f6c0a65ce7b59a6e4d4e5b6ddb70795e97afee
divine016 <koutidivine@gmail.com>
2023-05-19 15:12:40 +0100

    day 31

--- day31.js
+++ day31.js
@@ -7,3 +7,13 @@ function splitify(str) {
   }
   
   console.log(splitify("Hello World,I-am code"));
+
+  function sentensify(str) {
+    // Only change code below this line
+    let arr = str.split(/\W/)
+    return arr.join(' ')
+  
+    // Only change code above this line
+  }
+  
+  console.log(sentensify("May-the-force-be-with-you"));
