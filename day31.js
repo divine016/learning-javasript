e1f9232022475ec438ef35461d1c07f0e67abb29
divine016 <koutidivine@gmail.com>
2023-05-19 11:33:55 +0100

    day 31

---
+++ day31.js
@@ -0,0 +1,9 @@
+//using regular expressions and also the split method to split string in to an array of that string
+function splitify(str) {
+    // Only change code below this line
+      return str.split(/\W/);
+  
+    // Only change code above this line
+  }
+  
+  console.log(splitify("Hello World,I-am code"));
